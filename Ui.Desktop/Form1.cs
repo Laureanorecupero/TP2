f4fbd351cd0b03600506dd673e4a0375364ff076
Laureano Recúpero <laureano_14@hotmail.com>
2020-05-26 18:25:56 -0300

    Proyecy UI.Desktop

--- Ui.Desktop/Form1.cs
+++ Ui.Desktop/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Ui.Desktop
+namespace UI.Desktop
 {
     public partial class Form1 : Form
     {
@@ -16,6 +16,5 @@ namespace Ui.Desktop
         {
             InitializeComponent();
         }
-
     }
 }
